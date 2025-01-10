628ce8a3ed81e080b04cea7323e06d1b3879a190
mxmws <mewsmaxi@hu-berlin.de>
2025-01-10 18:27:44 +0100

    changed default events

--- src/constants.jsx
+++ src/constants.jsx
@@ -14,146 +14,188 @@ export const PLEASANT_COLORS = [
 ];
 
 export const CATEGORIES = [
-  { name: 'Location', color: '#F8D3A3' },
-  { name: 'Education', color: '#C5E6C5' },
-  { name: 'Work', color: '#B3D8EE' },
-  { name: 'Personal Life', color: '#F4B7B2' }
+  { name: 'Location', color: '#A3C9F1' },
+  { name: 'Education', color: '#BFD8BD' },
+  { name: 'Work', color: '#F4B7B2' },
+  { name: 'Personal Life', color: '#CBD5E0' },
+  { name: 'Tutorial', color: '#F8D3A3' }
 ];
 
 export const EXAMPLE_EVENTS = [
   // Location events
   {
     id: 1,
-    name: 'Lived in Italy',
+    name: 'Vacation Italy',
     category: 'Location',
-    start: 2020 * 12 + 8, // August 2017
-    duration: 6, // 4 years
-    hasDuration: true
-  },
-  {
-    id: 2,
-    name: 'Vacationed in Italy',
-    category: 'Location',
-    start: 2018 * 12 + 5, // May 2018
+    start: 2019 * 12 + 3,
     hasDuration: false
   },
   {
-    id: 3,
-    name: 'Vacationed in Australia',
+    id: 2,
+    name: 'Italy',
     category: 'Location',
-    start: 2021 * 12 + 11, // November 2021
+    start: 2020 * 12 + 1,
+    duration: 6,
+    hasDuration: true
+  },
+  {
+    id: 3,
+    name: 'Vacation Japan',
+    category: 'Location',
+    start: 2021 * 12 + 11,
     hasDuration: false
   },
   {
     id: 4,
-    name: 'Vacationed in Canada',
+    name: 'Denmark',
     category: 'Location',
-    start: 2023 * 12 + 2, // February 2023
+    start: 2023 * 12 + 6,
+    duration: 18,
+    hasDuration: true
+  },
+  {
+    id: 5,
+    name: 'Vacation Brasil',
+    category: 'Location',
+    start: 2023 * 12 + 1,
     hasDuration: false
   },
 
   // Education events
   {
-    id: 5,
-    name: 'College',
-    category: 'Education',
-    start: 2017 * 12 + 8, // August 2017
-    duration: 12*4, // 4 years
-    hasDuration: true
-  },
-  {
     id: 6,
-    name: 'Exchange Semester',
+    name: 'Bachelors Degree',
     category: 'Education',
-    start: 2020 * 12 + 8, // August 2017
-    duration: 6, // 4 years
+    start: 2018 * 12 + 9,
+    duration: 48,
     hasDuration: true
   },
   {
     id: 7,
-    name: 'Online Course in Data Science',
+    name: 'Exchange semester',
     category: 'Education',
-    start: 2022 * 12 + 1, // January 2022
-    duration: 3, // 6 months
+    start: 2020 * 12 + 1,
+    duration: 6,
     hasDuration: true
-  },
-  {
-    id: 8,
-    name: 'Attended Leadership Workshop',
-    category: 'Education',
-    start: 2022 * 12 + 11, // March 2023
-    hasDuration: false
   },
 
   // Work events
   {
+    id: 8,
+    name: 'Freelance work',
+    category: 'Work',
+    start: 2019 * 12 + 1,
+    duration: 24,
+    hasDuration: true
+  },
+  {
+    id: 9,
+    name: 'Internship',
+    category: 'Work',
+    start: 2021 * 12 + 5,
+    duration: 3,
+    hasDuration: true
+  },
+  {
     id: 10,
     name: 'First Job',
     category: 'Work',
-    start: 2021 * 12 + 10, // June 2020
-    duration: 21, // 1.5 years
+    start: 2023 * 12 + 6,
+    duration: 18,
     hasDuration: true
   },
   {
     id: 11,
-    name: 'Promoted',
+    name: 'Promotion',
     category: 'Work',
-    start: 2022 * 12 + 7, // January 2022
+    start: 2024 * 12 + 6,
     hasDuration: false
-  },
-  {
-    id: 12,
-    name: 'Freelancing',
-    category: 'Work',
-    start: 2023 * 12 + 7,
-    duration: 12,
-    hasDuration: true
-  },
-  {
-    id: 13,
-    name: 'Summer Internship',
-    category: 'Work',
-    start: 2020 * 12 + 5,
-    duration: 3,
-    hasDuration: true
   },
 
   // Personal Life events
   {
-    id: 14,
-    name: 'Got Married',
+    id: 12,
+    name: 'Got married',
     category: 'Personal Life',
-    start: 2020 * 12 + 6, // June 2020
+    start: 2020 * 12 + 6,
     hasDuration: false
+  },
+  {
+    id: 13,
+    name: 'Adopted cat',
+    category: 'Personal Life',
+    start: 2021 * 12 + 1,
+    hasDuration: false
+  },
+  {
+    id: 14,
+    name: 'Actively playing tennis',
+    category: 'Personal Life',
+    start: 2021 * 12 + 6,
+    duration: 24,
+    hasDuration: true
   },
   {
     id: 15,
-    name: 'Volunteering at Animal Shelter',
+    name: 'First child',
     category: 'Personal Life',
-    start: 2021 * 12 + 2, // February 2021
-    duration: 18, // 1.5 years
-    hasDuration: true
+    start: 2021 * 12 + 9,
+    hasDuration: false
   },
   {
     id: 16,
-    name: 'Started Gardening Hobby',
+    name: 'Cat got hit by tennis racket',
     category: 'Personal Life',
-    start: 2019 * 12 + 5, // May 2019
-    duration: 60, // 5 years
-    hasDuration: true
+    start: 2023 * 12 + 5,
+    hasDuration: false
   },
   {
     id: 17,
-    name: 'Adopted a Pet',
+    name: 'Second child',
     category: 'Personal Life',
-    start: 2022 * 12 + 11, // November 2022
+    start: 2024 * 12 + 4,
     hasDuration: false
   },
+
+  // Tutorial events
   {
-    id: 18,
-    name: 'Completed Marathon',
-    category: 'Personal Life',
-    start: 2023 * 12 + 9, // September 2023
-    hasDuration: false
+    id: 'w1',
+    name: 'Drag and drop events to changes dates...',
+    category: 'Tutorial',
+    start: 2020 * 12 + 0,
+    duration: 14,
+    hasDuration: true
+  },
+  {
+    id: 'w2',
+    name: 'You can also adjust the length',
+    category: 'Tutorial',
+    start: 2021 * 12 + 3,
+    duration: 12,
+    hasDuration: true
+  },
+  {
+    id: 'w3',
+    name: '...and rows',
+    category: 'Tutorial',
+    start: 2021 * 12 - 1,
+    duration: 3,
+    hasDuration: true
+  },
+  {
+    id: 'w7',
+    name: 'Click "Share Timeline" after you finish your timeline to create a specific URL',
+    category: 'Tutorial',
+    start: 2022 * 12 + 7,
+    duration: 28,
+    hasDuration: true
+  },
+  {
+    id: 'w8',
+    name: 'Click "Delete All Events" and start filling the categories with your own life :)',
+    category: 'Tutorial',
+    start: 2022 * 12 + 7,
+    duration: 28,
+    hasDuration: true
   }
 ];
